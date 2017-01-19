0409894b2d05339aaedab366933083bbb3df168a
Megan Truong <mtruong3@uw.edu>
2017-01-19 15:20:15 -0800

    Add code to exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,27 +2,28 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
-
+pts.scored <- c(12, 3, 37, 27, 26, 6, 20, 31, 31, 26, 5, 40, 10, 24, 31, 25)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+pts.against <- c(10, 9, 18, 17, 24, 6, 25, 25, 24, 15, 14, 7, 38, 3, 34, 23)
 
 # Combine your two vectors into a dataframe
-
+seahawks.scores <- data.frame(pts.scored, pts.against)
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+seahawks.scores["diff"] <- pts.scored - pts.against
 
 # Create a new column "won" which is TRUE if the Seahawks won
-
+seahawks.scores["won"] <- pts.scored > pts.against
 
 # Create a vector of the opponent names corresponding to the games played
-
+opponents <- c("Dolphins", "Ram", "49er", "Jets", "Falcons", "Cardinal", "Saints", "Bills", "Patriots", "Eagles", "Buccaneers", "Panthers", "Packers", "Rams", "Cardinals", "49ers")
 
 # Assign your dataframe rownames of their opponents
-
+rownames(seahawks.scores) <- opponents
 
 # View your data frame to see how it has changed!
+View(seahawks.scores)
